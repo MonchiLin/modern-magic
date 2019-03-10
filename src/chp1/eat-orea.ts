dc289a6260eeb85e87d49a4087aa26e93415a864
muxi2015 <lin912305043@gmail.com>
2019-03-10 17:23:52 +0800

    add

---
+++ src/chp1/eat-orea.ts
@@ -0,0 +1,8 @@
+function dipMilk(o) {
+    return {}
+}
+
+const orea = ["top", "middle", "Button"]
+// @ts-ignore
+const [top, ...middleAndButton] = orea
+const wetMiddleAndButtom = dipMilk(middleAndButton)
---
+++ src/chp1/try_out.clj
@@ -0,0 +1,2 @@
+(ns try-out)
+(vector [1 2 3 4])
