dd45aca657112c84152e9993d7a37cc64dd4e3ff
muxi2015 <lin912305043@gmail.com>
2019-04-20 20:22:30 +0800

    add

--- src/mostly-adequate-guide/chp8/index.ts
+++ src/mostly-adequate-guide/chp8/index.ts
@@ -1,4 +1,4 @@
-import {prop, compose, curry, concat, map, toString} from 'ramda'
+import {compose, concat, curry, eqProps, filter, head, last, map, prop, split, toString} from 'ramda'
 import {Either} from 'monet'
 import Moment from 'moment'
 
@@ -74,3 +74,94 @@ const zolatar = compose(
 zolatar({birthdate: '2005-12-12'})
 
 zolatar({birthdate: 'balllons!'})
+
+class IO {
+    private __value: any;
+
+    constructor(f) {
+        this.__value = f
+    }
+
+    static of(x) {
+        return new IO(() => x)
+    }
+
+    map(f) {
+        return new IO(compose(f, this.__value))
+    }
+
+}
+
+const ioWindow = IO.of(window)
+
+const width = ioWindow.map(win => win.innerWidth)
+
+const sets = ioWindow.map(
+    compose(
+        split("/"),
+        prop("href"),
+        prop("location")
+    )
+)
+
+const $ = selector => IO.of(document.querySelectorAll(selector))
+
+// const div = $("div").map(head).map(div => div.innerHTML)
+
+const url = IO.of(window.location.href)
+
+const toPairs = compose(
+    map(
+        split("="),
+        split("&")
+    )
+)
+
+const param = compose(
+    toPairs,
+    last,
+    split('?')
+)
+
+const findParam = (key) => fmap(compose(
+    Maybe.of,
+    filter(
+        compose(
+            eqProps(key),
+            head
+        )
+    ),
+    param,
+), url)
+
+// 5 * 2 + 5 * 3 === 5 * (2 + 3)
+
+class Container {
+    constructor(x: any) {
+        this.__value = x
+    }
+
+    __value: any;
+
+
+    static of(x) {
+        new Container(x)
+    }
+}
+
+const id = x => x
+
+// const idLaw1 = map(id)
+// const idLaw2 = id
+// console.log(idLaw1(Container.of(2)))
+// console.log(idLaw2(Container.of(2)))
+
+
+
+
+
+
+
+
+
+
