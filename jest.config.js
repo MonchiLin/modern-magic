798fad78a267905d07e20936de017d283270d533
MonchiLin <lin912305043@gmail.com>
2019-10-18 20:37:10 +0800

    feat(Jest):新增用于测试的 tsconfig

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,23 @@
+const path = require("path")
+
 module.exports = {
-  preset: 'ts-jest',
-  transform: {
-    "^.+\\.jsx?$": "babel-jest", // Adding this line solved the issue
-    "^.+\\.tsx?$": "ts-jest"
-  },
-  testEnvironment: 'node',
-  globals: {
-    'ts-jest': {
-      // 通过这段代码可以判断 tsconfig 与 jest 某些配置冲突了, 待详细检查
-      // https://github.com/kulshekhar/ts-jest/issues/937
-      tsConfig: false
+    preset: 'ts-jest',
+    transform: {
+        "^.+\\.jsx?$": "babel-jest",
+        "^.+\\.tsx?$": "ts-jest"
+    },
+    testEnvironment: 'node',
+    rootDir: __dirname,
+    testMatch: [
+        '<rootDir>/projects/**/__tests__/**/*spec.[jt]s?(x)',
+        '<rootDir>/projects/Daily-Interview-Question/**/*.ts',
+        '<rootDir>/blog/**/*.[jt]s?(x)',
+    ],
+    globals: {
+        'ts-jest': {
+            // 通过这段代码可以判断 tsconfig 与 jest 某些配置冲突了, 待详细检查
+            // https://github.com/kulshekhar/ts-jest/issues/937
+            tsConfig: path.join(__dirname, 'tsconfig.spec.json')
+        }
     }
-  }
 };
