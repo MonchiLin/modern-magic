b5eddd14fcc5650c8fe861e2cebfbc03914476aa
MonchiLin <lin912305043@gmail.com>
2019-11-13 13:25:48 +0800

    feat: 新增归并排序的 TS 实现

---
+++ projects/Data-Structure-and-Algorithms/Algorithms/Sort/__tests__/MergeSort.spec.ts
@@ -0,0 +1,11 @@
+import mergeSort from "../mergeSort";
+
+describe('mergeSort', function () {
+    it('should sort', function () {
+
+        const arr = [32, 12, 56, 78, 76, 45, 36]
+        const result = mergeSort(arr)
+
+        expect(result).toStrictEqual([12, 32, 36, 45, 56, 76, 78])
+    });
+});
