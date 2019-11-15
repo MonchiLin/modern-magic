aeb4a6e307f4d3867e2b3813f011bf13f9c45d22
MonchiLin <lin912305043@gmail.com>
2019-11-15 13:35:32 +0800

    feat(算法): 新增随机排序算法

---
+++ projects/Data-Structure-and-Algorithms/Algorithms/Sort/__tests__/random.spec.ts
@@ -0,0 +1,7 @@
+import {randomSort} from '../randomSort'
+
+describe('测试 randomSort', function () {
+    it('case 1', function () {
+        console.log(randomSort([1, 2, 3, 4, 5, 7, 10]))
+    });
+});
