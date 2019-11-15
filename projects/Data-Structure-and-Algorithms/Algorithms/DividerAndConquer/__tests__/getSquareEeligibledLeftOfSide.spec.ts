bd9e2c59d2d21e080b345d161e636d1e746f8503
MonchiLin <lin912305043@gmail.com>
2019-11-15 13:10:16 +0800

    feat(算法): 新增分而治之算法的应用

---
+++ projects/Data-Structure-and-Algorithms/Algorithms/DividerAndConquer/__tests__/getSquareEeligibledLeftOfSide.spec.ts
@@ -0,0 +1,11 @@
+import {getSquareEeligibledLeftOfSide} from "../getSquareEeligibledLeftOfSide";
+
+describe('getSquareEeligibledLeftOfSide', function () {
+
+    it('should get side', function () {
+
+        getSquareEeligibledLeftOfSide(1024, 1200)
+
+    });
+
+});
