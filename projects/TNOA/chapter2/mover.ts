573baf08adb39204ea15f55aa4fad6c6e1bc0b81
MonchiLin <lin912305043@gmail.com>
2019-12-15 22:04:05 +0800

    feat(TNOA): 修正目录

--- projects/TNOA/chapter2/mover.ts
+++ projects/TNOA/chapter2/mover.ts
@@ -1,47 +1,19 @@
-import PVector from "./pvector";
+import p from 'p5'
 
 class Mover {
-  location: PVector
-  velocity: PVector
-  acceleration: PVector
-  topspeed = 10
+  location: p.Vector
+  velocity: p.Vector
+  acceleration: p.Vector
 
-  constructor(private p) {
-    this.location = new PVector(p.width * 0.5, p.height * 0.5)
-    this.velocity = new PVector(0, 0)
-    this.acceleration = new PVector(-0.01, 0.1)
-  }
-
-  checkEdges() {
-    if (this.location.x > this.p.width) {
-      this.location.x = 0
-    } else if (this.location.x < 0) {
-      this.location.x = this.p.width
-    }
-
-    if (this.location.y > this.p.height) {
-      this.location.y = 0
-    } else if (this.location.y < 0) {
-      this.location.y = this.p.height
-    }
+  constructor() {
 
   }
 
-  update() {
-    this.velocity.add(this.acceleration)
-    this.velocity.limit(this.topspeed)
-    console.log(this.velocity.y)
+  applyForce() {
 
-    this.location.add(this.velocity)
-  }
-
-  display() {
-    this.p.stroke(0)
-    this.p.fill(175)
-    this.p.ellipse(this.location.x, this.location.y, 16, 16)
   }
 
 }
 
-export default Mover
 
+export default Mover
