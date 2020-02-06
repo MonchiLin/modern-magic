297b2141ef28de18cc376e6daa94698b9ec3123d
MonchiLin <lin912305043@gmail.com>
2020-02-07 06:18:16 +0800

    chore(TNOA): 增加练习代码

---
+++ projects/TNOA/chapter4/particle-system-405/sketch.ts
@@ -0,0 +1,31 @@
+import "p5"
+import ParticleSystem from "./particel-system";
+
+let particleSystems: ParticleSystem[] = []
+
+function setup() {
+  createCanvas(640, 640)
+}
+
+let y = 0
+
+function draw() {
+  background(255)
+  particleSystems.forEach(ps => {
+    ps.applyForce(createVector(0, 0.08))
+    ps.run()
+  })
+}
+
+function mousePressed() {
+  particleSystems.push(new ParticleSystem(createVector(mouseX, mouseY)))
+}
+
+// @ts-ignore
+window.setup = setup
+
+// @ts-ignore
+window.draw = draw
+
+// @ts-ignore
+window.mousePressed = mousePressed
