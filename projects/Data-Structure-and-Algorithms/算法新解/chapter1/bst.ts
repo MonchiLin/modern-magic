78a932ddf9aac85746d5eb7f000b8bd96d9349ec
MonchiLin <lin912305043@gmail.com>
2020-03-25 12:37:12 +0800

    chore

--- projects/Data-Structure-and-Algorithms/算法新解/chapter1/bst.ts
+++ projects/Data-Structure-and-Algorithms/算法新解/chapter1/bst.ts
@@ -1,4 +1,4 @@
-import {range} from "ramda";
+import { range } from "ramda";
 
 class BST {
   root: Node
@@ -13,23 +13,25 @@ class BST {
       return
     }
 
-    this.insertKey(key, this.root)
+    this.insertKey(key, this.root, this.root)
   }
 
-  private insertKey(key: number, root: Node) {
-    const node = new Node(key)
+  private insertKey(key: number, node: Node, parent: Node) {
+    this.root
+    const newNode = new Node(key)
+    newNode.parent = parent
 
-    if (key > root.value) {
-      if (root.right == null) {
-        root.right = node
+    if (key > node.value) {
+      if (node.right == null) {
+        node.right = newNode
       } else {
-        this.insertKey(key, root.right)
+        this.insertKey(key, node.right, node)
       }
-    } else if (key < root.value) {
-      if (root.left == null) {
-        root.left = node
+    } else if (key < node.value) {
+      if (node.left == null) {
+        node.left = newNode
       } else {
-        this.insertKey(key, root.left)
+        this.insertKey(key, node.left, node)
       }
     }
   }
@@ -53,11 +55,86 @@ class BST {
 
   }
 
+  remove(key) {
+    let node = this.lookup(key)
+    if (!node) {
+      return
+    }
+    const parent = node.parent
+    const left = node.left
+    parent.left = left
+    left.parent = parent
+  }
+
+
+  lookup(key) {
+    return this.lookupNode(key, this.root)
+  }
+
+  lookupNode(key, node) {
+    while (node && node.value != null) {
+      if (node.value === key) {
+        return node
+      }
+      const left = this.lookupNode(key, node.left)
+      if (left != null) {
+        return left
+      } else {
+        return this.lookupNode(key, node.right)
+      }
+    }
+  }
+
+  succ(key) {
+    let node = this.lookup(key)
+    if (!node) {
+      return
+    }
+
+    if (node.right) {
+      return this.minByNode(node.right)
+    }
+
+    node = node.parent
+
+    while (node) {
+      if (node.value > key) {
+        return node
+      } else {
+        node = node.parent
+      }
+    }
+
+  }
+
+  get min() {
+    return this.minByNode(this.root)
+  }
+
+  minByNode(node) {
+    while (node.left) {
+      node = node.left
+    }
+    return node
+  }
+
+  maxByNode(node) {
+    while (node.right) {
+      node = node.right
+    }
+    return node
+  }
+
+  get max() {
+    return this.maxByNode(this.root)
+  }
+
 }
 
 class Node {
   left: Node
   right: Node
+  parent: Node
   value: number
 
   constructor(value?) {
@@ -68,20 +145,21 @@ class Node {
 describe("BST", () => {
   it('1', () => {
     const bst = new BST()
-    bst.insert(4)
-    bst.insert(3)
+    bst.insert(6)
     bst.insert(1)
-    bst.insert(2)
-
-
-    bst.insert(8)
     bst.insert(7)
-    bst.insert(16)
-    bst.insert(10)
-    bst.insert(9)
-    bst.insert(14)
+    bst.insert(5)
 
-    bst.traver(node => console.log(node))
+
+    bst.insert(3)
+    bst.insert(2)
+    bst.insert(4)
+    bst.insert(9)
+    bst.insert(8)
+    bst.insert(10)
+
+    const x = bst.lookup(5)
+    const succ = bst.succ(5)
   });
 
 
