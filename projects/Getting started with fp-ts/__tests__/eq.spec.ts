8ffc51bcb6c82f640361ce121c5f154b67ff2311
MonchiLin <lin912305043@gmail.com>
2020-03-26 16:04:28 +0800

    chore

--- projects/Getting started with fp-ts/__tests__/eq.spec.ts
+++
@@ -1,9 +0,0 @@
-import { elem, eqNumber } from "../eq";
-
-describe('elem', function () {
-  it('eqNumber', function () {
-    const elemNumber = elem(eqNumber)
-    expect(elemNumber(1, [1, 2, 3, 4])).toStrictEqual(true)
-  });
-
-});
--- projects/algorithms-fp/utility.ts
+++
@@ -1,5 +0,0 @@
-import {clone} from 'ramda'
-
-export {
-    clone
-}
