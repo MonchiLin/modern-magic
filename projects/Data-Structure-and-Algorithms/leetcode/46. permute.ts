fbe8eaa3475af268be172602c15b29dce570f559
MonchiLin <lin912305043@gmail.com>
2020-04-03 16:13:49 +0800

    chore

--- projects/Data-Structure-and-Algorithms/leetcode/46. permute.ts
+++ projects/Data-Structure-and-Algorithms/leetcode/46. permute.ts
@@ -2,6 +2,8 @@
 // 链接：https://leetcode-cn.com/problems/permutations/
 // 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
+// TODO 未完成
+
 
 describe("solution", () => {
   it('1', () => {
--- projects/playground/pp.vue
+++ projects/playground/pp.vue
@@ -6,41 +6,6 @@
 
 <script>
 
-  const user = {
-    id: 888,
-    name: 'JerryHong',
-    courseLists: [{
-      "name": "My Courses",
-      "courses": [{
-        "id": 511019,
-        "title": "React for Beginners",
-        "coverPng": "https://res.cloudinary.com/dohtkyi84/image/upload/v1481226146/react-cover.png",
-        "tags": [{id: 1, name: "JavaScript"}],
-        "rating": 5
-      }, {
-        "id": 511020,
-        "title": "Front-End automat workflow",
-        "coverPng": "https://res.cloudinary.com/dohtkyi84/image/upload/v1481226146/react-cover.png",
-        "tags": [{"id": 2, "name": "gulp"}, {"id": 3, "name": "webpack"}],
-        "rating": 4
-      }]
-    }, {
-      "name": "New Release",
-      "courses": [{
-        "id": 511022,
-        "title": "Vue2 for Beginners",
-        "coverPng": "https://res.cloudinary.com/dohtkyi84/image/upload/v1481226146/react-cover.png",
-        "tags": [{id: 1, name: "JavaScript"}],
-        "rating": 5
-      }, {
-        "id": 511023,
-        "title": "Angular2 for Beginners",
-        "coverPng": "https://res.cloudinary.com/dohtkyi84/image/upload/v1481226146/react-cover.png",
-        "tags": [{id: 1, name: "JavaScript"}],
-        "rating": 4
-      }]
-    }]
-  };
 
   const concatAll = () => {
     const result = [];
