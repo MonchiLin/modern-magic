053b3bbb0315db767816f4db4a681c31d4dbe244
MonchiLin <lin912305043@gmail.com>
2024-05-14 23:34:03 +0800

    d:

--- three-journey/shader-patterns/src/main.ts
+++ three-journey/shader-patterns/src/main.ts
@@ -123,6 +123,198 @@ const patterns = [
 
       void main() {
         float strength = mod((vUv.y * 10.0), 1.0);
+        // 如果小于 0.5 则返回 0， 否则返回 1 （也就是说大于等于 0.5 都返回 1）
+        strength = step(0.5, strength);
+        gl_FragColor = vec4(strength, strength, strength, 1.0);
+      }
+    `
+  },
+  // pattern10
+  {
+    vertexShader: `
+      varying vec2 vUv;
+
+      void main() {
+        vUv = uv;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+      }
+    `,
+    fragmentShader: `
+      varying vec2 vUv;
+
+      void main() {
+        float strength = mod((vUv.x * 10.0), 1.0);
+        // 如果小于 0.5 则返回 0， 否则返回 1 （也就是说大于等于 0.5 都返回 1）
+        strength = step(0.5, strength);
+        gl_FragColor = vec4(strength, strength, strength, 1.0);
+      }
+    `
+  },
+  // pattern11
+  {
+    vertexShader: `
+      varying vec2 vUv;
+
+      void main() {
+        vUv = uv;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+      }
+    `,
+    fragmentShader: `
+      varying vec2 vUv;
+
+      void main() {
+        // 设想， 上面的 x 和 y 经过重新映射范围 0~1 -> 0~10 -> [0...0, 1...1, 0....0, 1...1]
+        // 假设渲染坐标为 (0, 1) 那么结果为 (0, 1)  X 轴无颜色， Y 轴有颜色
+        // 假设渲染坐标为 (0, 0) 那么结果为 (0, 0)  X 轴有颜色， Y 轴无颜色
+        // 假设渲染坐标为 (1, 1) 那么结果为 (1, 1)  X 轴有颜色， Y 轴乌颜色
+        float strength = step(0.5, mod((vUv.x * 10.0), 1.0));
+        strength += step(0.5, mod((vUv.y * 10.0), 1.0));
+
+        gl_FragColor = vec4(strength, strength, strength, 1.0);
+      }
+    `
+  },
+  // pattern12
+  {
+    vertexShader: `
+      varying vec2 vUv;
+
+      void main() {
+        vUv = uv;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+      }
+    `,
+    fragmentShader: `
+      varying vec2 vUv;
+
+      void main() {
+        float strength = step(0.8, mod((vUv.x * 10.0), 1.0));
+        // 秘诀在于如果上面是 0，这里必定也是 0
+        strength *= step(0.8, mod((vUv.y * 10.0), 1.0));
+
+        gl_FragColor = vec4(strength, strength, strength, 1.0);
+      }
+    `
+  },
+  // pattern14
+  {
+    vertexShader: `
+      varying vec2 vUv;
+
+      void main() {
+        vUv = uv;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+      }
+    `,
+    fragmentShader: `
+      varying vec2 vUv;
+
+      void main() {
+        float barX = step(0.4, mod((vUv.x * 10.0), 1.0));
+        barX *= step(0.8, mod((vUv.y * 10.0), 1.0));
+        float barY = step(0.8, mod((vUv.x * 10.0), 1.0));
+        barY *= step(0.4, mod((vUv.y * 10.0), 1.0));
+
+        float strength = barX + barY;
+
+        gl_FragColor = vec4(strength, strength, strength, 1.0);
+      }
+    `
+  },
+  // pattern15
+  {
+    vertexShader: `
+      varying vec2 vUv;
+
+      void main() {
+        vUv = uv;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+      }
+    `,
+    fragmentShader: `
+      varying vec2 vUv;
+
+      void main() {
+        float barX = step(0.4, mod((vUv.x * 10.0), 1.0));
+        barX *= step(0.8, mod((vUv.y * 10.0) + 0.2, 1.0));
+
+        float barY = step(0.8, mod((vUv.x * 10.0) + 0.2, 1.0));
+        barY *= step(0.4, mod((vUv.y * 10.0), 1.0));
+
+        float strength = barX + barY;
+
+        gl_FragColor = vec4(strength, strength, strength, 1.0);
+      }
+    `
+  },
+  // pattern16
+  {
+    vertexShader: `
+      varying vec2 vUv;
+
+      void main() {
+        vUv = uv;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+      }
+    `,
+    fragmentShader: `
+      varying vec2 vUv;
+
+      void main() {
+        // 这里其实是把 0~1 映射到 0.5 ~ 0 ~ 0.5
+        // 0.0 -> 0.5
+        // 0.1 -> 0.4
+        // 0.2 -> 0.3
+        // 0.3 -> 0.2
+        // 0.4 -> 0.1
+        // 0.5 -> 0.0
+        // 0.6 -> 0.1
+        // 0.7 -> 0.2
+        // 0.8 -> 0.3
+        // 0.9 -> 0.4
+        // 1.0 -> 0.5
+        float strength = abs(vUv.x - 0.5);
+
+        gl_FragColor = vec4(strength, strength, strength, 1.0);
+      }
+    `
+  },
+  // pattern17
+  {
+    vertexShader: `
+      varying vec2 vUv;
+
+      void main() {
+        vUv = uv;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+      }
+    `,
+    fragmentShader: `
+      varying vec2 vUv;
+
+      void main() {
+        float strength = min(abs(vUv.x - 0.5), abs(vUv.y - 0.5));
+        gl_FragColor = vec4(strength, strength, strength, 1.0);
+      }
+    `
+  },
+  // pattern18
+  {
+    vertexShader: `
+      varying vec2 vUv;
+
+      void main() {
+        vUv = uv;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+      }
+    `,
+    fragmentShader: `
+      varying vec2 vUv;
+
+      void main() {
+        float strength = max(abs(vUv.x - 0.5), abs(vUv.y - 0.5));
+
         gl_FragColor = vec4(strength, strength, strength, 1.0);
       }
     `
@@ -194,5 +386,10 @@ new FontLoader().load('/Roboto_Bold.json', font => {
       font
     })
   })
-
+  setTimeout(() =>{
+    document.body.scrollTo({
+      left: 0,
+      top: Number.MAX_VALUE
+    })
+  })
 })
